0faf47af8d71af5aac701fe035459919b35e8e92
redouter <skittels261105@gmail.com>
2025-11-16 15:51:48 +0300

    feat: add Entity and Enum for Users

---
+++ Users/Users.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.Domain.Enums;
+
+namespace Users.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsActive {  get; set; }
+        public bool EmailConfirmed { get; set; } = false;
+    }
+}
