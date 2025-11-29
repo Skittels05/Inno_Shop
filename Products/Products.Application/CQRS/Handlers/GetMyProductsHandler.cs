06a58cf3f6f44b8b08b36248752496e91a8c1ec2
redouter <skittels261105@gmail.com>
2025-11-29 04:19:59 +0300

    feat: add handlers for get products

---
+++ Products/Products.Application/CQRS/Handlers/GetMyProductsHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Products.Application.CQRS.Queries;
+using Products.Application.DTOs;
+using Products.Domain.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Products.Application.CQRS.Handlers
+{
+    public class GetMyProductsHandler : IRequestHandler<GetMyProductsQuery, IEnumerable<ProductDto>>
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetMyProductsHandler(IProductRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDto>> Handle(GetMyProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _repository.GetProductsByUserAsync(request.UserId);
+            return _mapper.Map<IEnumerable<ProductDto>>(products);
+        }
+    }
+}
---
+++ Products/Products.Application/CQRS/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Products.Application.CQRS.Queries;
+using Products.Application.DTOs;
+using Products.Domain.Interfaces.Repositories;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Products.Application.CQRS.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, ProductDto>
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetProductByIdHandler(IProductRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDto> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _repository.GetByIdAsync(request.ProductId, request.UserId);
+
+            if (product == null)
+                return null;
+
+            return _mapper.Map<ProductDto>(product);
+        }
+    }
+}
